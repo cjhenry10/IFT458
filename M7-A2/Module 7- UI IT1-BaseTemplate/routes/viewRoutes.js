f7b62e94ad120a09a1c7fec84681f99eb7542ab9
Connor Henry <cjhenry10@gmail.com>
2022-11-21 17:19:28 -0800

    modular pug files

---
+++ M7-A2/Module 7- UI IT1-BaseTemplate/routes/viewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+
+const router = express.Router();
+
+router.get('/', viewsController.getLandingPAge);
+router.get('/allcourses', viewsController.getAllCourses);
+router.get('/allloans', viewsController.getAllLoans);
+router.get('/Course', viewsController.getCourse);
+router.get('/login', viewsController.getLoginForm);
+router.get('/signIn', viewsController.getSignInForm);
+router.get('/loginUser', viewsController.getLoginUser);
+
+
+module.exports = router;
